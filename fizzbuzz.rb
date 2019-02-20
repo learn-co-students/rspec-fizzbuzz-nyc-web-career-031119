b80dbe30139426a67f74716eb63d651f86b5d6e7
Kane Koo <kanekoo@gmail.com>
2019-02-20 20:42:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(var)
+ # uts "WTF  #{var}"
+  if ((var % 15) == 0)
+    return ("FizzBuzz")
+  elsif ((var % 3) == 0) 
+    return ("Fizz")
+  elsif ((var % 5) == 0)
+    return ("Buzz")
+  else
+    return(NIL)
+  end
+end
