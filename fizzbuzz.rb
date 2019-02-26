95260e3ca90f06214e08649fb6c3d9dc4e86e97d
github username <github email address>
2019-02-26 14:37:19 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num%3 == 0 && num%5 == 0
+    'FizzBuzz'
+  elseif num%3 == 0
+    'Fizz'
+  elseif num%5 == 0
+    'Buzz'
+  else
+    nil
+end
